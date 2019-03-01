4d22ddb578d0928068aa47784c14c437c6e71716
Ryo Ota <nwtgck@gmail.com>
2019-03-01 15:13:33 +0900

    [add] Port backup() function to Go language

--- main.go
+++ main.go
@@ -3,12 +3,54 @@ package main
 import (
 	"fmt"
 	"github.com/nwtgck/ibk/util"
+	"os"
 	"path/filepath"
 	"sort"
+	"time"
 )
 
-// TODO: impl
-func backup(){
+// Format time for file name
+func formatTime(t time.Time) string {
+	zone, _ := t.Zone()
+	// NOTE: Tried to use time#Format(), but it is very weird implementation.
+	// Third-party libraries seem not to be maintained.
+	return fmt.Sprintf(
+		"%d%02d%02d_%02d%02d_%02d_%03d_%s",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(),
+		t.Second(),
+		t.Nanosecond() / 1000000,
+		zone,
+	)
+}
+
+func backup() error {
+	// TODO: Hard code
+	targetDirPath := "./mydir"
+	// TODO: Hard code
+	dstDirPath := "./mydst"
+	// Get the base name of the target directory
+	targetBaseName := filepath.Base(targetDirPath)
+	// Difine snar name
+	targetSnarName := fmt.Sprintf("%s.snar", targetBaseName)
+
+	// Create destination path if it doesn't exist
+	os.MkdirAll(dstDirPath, os.ModePerm)
+
+	// Define tar file name
+	tarFileName := fmt.Sprintf("%s_%s.tar", targetBaseName, formatTime(time.Now()))
+
+	targetSnarpath := filepath.Join(dstDirPath, targetSnarName)
+	tarFilePath := filepath.Join(dstDirPath, tarFileName)
+
+	// Incremental backup
+	_, err := util.EchoRunCmdStr(fmt.Sprintf(
+		"gtar -g %s -cf %s %s",
+		targetSnarpath,
+		tarFilePath,
+		targetDirPath,
+	))
+	return err
 }
 
 func restore() error {
@@ -43,5 +85,6 @@ func restore() error {
 }
 
 func main() {
-	restore()
+	backup()
+	//restore()
 }
